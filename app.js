0e0d0e2679be1de396e62082826645390d7ae3df
ayeshasaleem24 <143605936+ayeshasaleem24@users.noreply.github.com>
2025-04-12 16:56:57 +0500

    app.js

--- app.js
+++ app.js
@@ -13,27 +13,3 @@ function menutoggle(){
         MenuItems.style.maxHeight = "0px"
     }
 }
-
-
-
-// // <!-- --------------------------------- JS for Product Gallery --------------------- -->
-
-// var ProductImg = document.getElementById("ProductImg");
-// var SmallImg = document.getElementById("small-img");
-
-// SmallImg[0].onclick = function()
-// {
-//     ProductImg.src = SmallImg[0].src;
-// }
-// SmallImg[1].onclick = function()
-// {
-//     ProductImg.src = SmallImg[1].src;
-// }
-// SmallImg[2].onclick = function()
-// {
-//     ProductImg.src = SmallImg[2].src;
-// }
-// SmallImg[3].onclick = function()
-// {
-//     ProductImg.src = SmallImg[3].src;
-// }
